bbff70a13b0fb3975f103e87227831acf55bee9c
galla192 <galla192@msu.edu>
2015-03-01 18:32:14 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require 'HolidApi'
+
+class MyWebApp < Sinatra::Base
+	get '/' do
+		@holidays_born=HolidApi.get(year: 1991, month: 11)
+		@holidays_now=HolidApi.get(year: Time.now.year, month: Time.now.month)
+		erb :index1
+	end
+end
