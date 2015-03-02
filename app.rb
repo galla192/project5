4519eb68f6c406861991907e7f36df37e68ae5d3
galla192 <galla192@msu.edu>
2015-03-01 20:06:53 -0500

    forgot the 1 on app.rb

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ require 'HolidApi'
 class MyWebApp < Sinatra::Base
   get '/' do
   	@holidays = HolidApi.get(year: params['year'], month: params['month'], day: params['day'])
-    erb :index
+    erb :index1
   end
 end
