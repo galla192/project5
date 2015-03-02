0333a464d5b0465ee4ac2c0c9876d61026b6a4d4
galla192 <galla192@msu.edu>
2015-03-01 20:09:15 -0500

    retry

--- app.rb
+++ app.rb
@@ -3,7 +3,19 @@ require 'HolidApi'
 
 class MyWebApp < Sinatra::Base
   get '/' do
-  	@holidays = HolidApi.get(year: params['year'], month: params['month'], day: params['day'])
+  	if params['year'] != nil && params['year'] != ""
+  		if params['month'] != nil && params['month'] != ""
+  			if params['day'] != nil && params['day'] != ""
+  				@holidays = HolidApi.get(year: params['year'], month: params['month'], day: params['day'])
+  			else
+  				@holidays = HolidApi.get(year: params['year'], month: params['month'])
+  			end
+  		else
+  			@holidays = HolidApi.get(year: params['year'])
+  		end
+    else
+    	@holidays = HolidApi.get(year: (Date.today.strftime('%Y')).to_i)
+    end
     erb :index1
   end
 end
